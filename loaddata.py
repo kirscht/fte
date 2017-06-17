8bf118fdb7f3db698fff6e27577136809f81abe5
Korben <admin@octt.co>
2017-06-16 19:16:04 -0700

    Initial

--- loaddata.py
+++ loaddata.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import sys
+
 print "hello world"
 
 fi = open("/home/kkirscht/Downloads/Mailing_CurrentSheet1_20170616.tsv","r")
@@ -8,6 +10,6 @@ for input in fi:
     record =  input.split('\t')
 
     for item in record:
-        print(item, end="")
+        sys.write(item)
 
 fi.close()
