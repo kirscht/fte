43ac192dd25ed54f45dbfc262a4078585aedf215
Korben <admin@octt.co>
2017-06-16 19:32:54 -0700

    Initial

--- loaddata.py
+++ loaddata.py
@@ -8,7 +8,10 @@ for input in fi:
     record =  input.rstrip().split('\t')
 
     for item in record:
-        sys.stdout.write(item)
+        sys.stdout.write("%s\t" % item)
 
     sys.stdout.write('\n')
 fi.close()
+
+#ShortPropertyAddressZipcodeToNameAddresseeStreetCityStateZip
+#151 Canyoncrest07052-4565GhanshyamGhanshyam & Pushpa Lalla4 Oconnor CirWest Orange, NJ 07052-4565
