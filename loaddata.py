402593185118fc2ce28effe8261552edc5df5f95
Korben <admin@octt.co>
2017-06-16 19:23:43 -0700

    Initial

--- loaddata.py
+++ loaddata.py
@@ -4,8 +4,8 @@ import sys
 
 fi = open("/home/kkirscht/Downloads/Mailing_CurrentSheet1_20170616.tsv","r")
 
-for input in fi.rstrip():
-    record =  input.split('\t')
+for input in fi:
+    record =  input.rstrip().split('\t')
 
     for item in record:
         sys.stdout.write(item)
